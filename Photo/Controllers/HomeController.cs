afab8257dd3cdbe9a2a3418c0ecf7298b49f20d8
Nastia <gnatyk.anastasia@gmail.com>
2014-12-12 17:19:10 +0300

    first commit

---
+++ Photo/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Services.Description;
+
+namespace Photo.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult SaveDropzoneJsUploadedFiles()
+        {
+            bool isSaveSuccessfully = false;
+            foreach (string fileName in Request.Files)
+            {
+                HttpPostedFileBase file = Request.Files[fileName];
+
+
+                isSaveSuccessfully = true;
+            }
+            return Json(new {Message = string.Empty });
+        }
+
+    }
+}
